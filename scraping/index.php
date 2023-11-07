88cd7077d6654c3069782ae9177a712d951e2273
benjaminpaniagua <benjaminjosepaniaguarojas@gmail.com>
2023-11-07 15:57:40 -0600

    switch namel

--- scraping/index.php
+++ scraping/index.php
@@ -79,7 +79,7 @@
 //         $rand = rand(1 * 10, 70 * 10) / 10;
 //         $database->insert("tb_dish", [
 //             "id_category" => $id_category,
-//             "name_l" => $menu_item_names[$i],
+//             "namel" => $menu_item_names[$i],
 //             "description" => $menu_item_descriptions[$i],
 //             "img_recorted" => $image_urls[$i],
 //             "price" => $rand
@@ -171,7 +171,7 @@
 //         $rand = rand(1 * 10, 70 * 10) / 10;
 //         $database->insert("tb_dish", [
 //             "id_category" => $id_category,
-//             "name_l" => $menu_item_names[$i],
+//             "namel" => $menu_item_names[$i],
 //             "description" => $menu_item_descriptions[$i],
 //             "img_recorted" => $image_urls[$i],
 //             "price" => $rand
@@ -263,7 +263,7 @@
 //         $rand = rand(1 * 10, 70 * 10) / 10;
 //         $database->insert("tb_dish", [
 //             "id_category" => $id_category,
-//             "name_l" => $menu_item_names[$i],
+//             "namel" => $menu_item_names[$i],
 //             "description" => $menu_item_descriptions[$i],
 //             "img_recorted" => $image_urls[$i],
 //             "price" => $rand
@@ -277,91 +277,91 @@
 <!--  DRINKS -->
 <!--  DRINKS -->
 <?php
-require_once '../database.php';
+// require_once '../database.php';
 
-include('simple_html_dom.php');
+// include('simple_html_dom.php');
 
-//link
-$link = "https://www.allrecipes.com/recipes/77/drinks/";
+// //link
+// $link = "https://www.allrecipes.com/recipes/77/drinks/";
 
-$filenames = [];
-$menu_item_names = [];
-$menu_item_descriptions = [];
-$image_urls = [];
+// $filenames = [];
+// $menu_item_names = [];
+// $menu_item_descriptions = [];
+// $image_urls = [];
 
-$menu_items = 6;
-$id_category = 4;
+// $menu_items = 6;
+// $id_category = 4;
 
-$items = file_get_html($link);
+// $items = file_get_html($link);
 
-//save meals info and filenames for the images
-foreach ($items->find('.card--no-image') as $item) {
+// //save meals info and filenames for the images
+// foreach ($items->find('.card--no-image') as $item) {
 
-    $title = $item->find('.card__title-text');
-    $details = file_get_html($item->href);
-    $description = $details->find('.article-subheading');
-    $image = $details->find('.primary-image__image');
+//     $title = $item->find('.card__title-text');
+//     $details = file_get_html($item->href);
+//     $description = $details->find('.article-subheading');
+//     $image = $details->find('.primary-image__image');
 
-    if (count($image) > 0) {
-        $image_urls[] = $image[0]->src;
-    } else {
-        $replace_img = $item->find('.universal-image__image');
-        if (count($replace_img) > 0) {
-            $image_urls[] = $replace_img[0]->{'data-src'};
-        }
-    }
+//     if (count($image) > 0) {
+//         $image_urls[] = $image[0]->src;
+//     } else {
+//         $replace_img = $item->find('.universal-image__image');
+//         if (count($replace_img) > 0) {
+//             $image_urls[] = $replace_img[0]->{'data-src'};
+//         }
+//     }
 
-    if (count($description) > 0) {
-        if ($menu_items == 0) break;
+//     if (count($description) > 0) {
+//         if ($menu_items == 0) break;
 
-        $menu_item_names[] = trim($title[0]->plaintext);
-        $menu_item_descriptions[] = $description[0]->plaintext;
+//         $menu_item_names[] = trim($title[0]->plaintext);
+//         $menu_item_descriptions[] = $description[0]->plaintext;
 
-        $filename = strtolower(trim($title[0]->plaintext));
-        $filename = str_replace(' ', '-', $filename);
-        $filenames[] = $filename;
+//         $filename = strtolower(trim($title[0]->plaintext));
+//         $filename = str_replace(' ', '-', $filename);
+//         $filenames[] = $filename;
 
-        $menu_items--;
-    }
-}
+//         $menu_items--;
+//     }
+// }
 
-foreach ($filenames as $i => $item) {
-    echo "......................";
-    echo "<br>";
-    echo $item;
-    echo "<br>";
-    echo $menu_item_names[$i];
-    echo "<br>";
-    echo $menu_item_descriptions[$i];
-    echo "<br>";
-    echo $image_urls[$i];
-    echo "<br>";
-    echo rand(1 * 10, 70 * 10) / 10;
-    echo "<br>";
-    //$menu_items--;
-    //if($menu_items == 0) break;
-}
+// foreach ($filenames as $i => $item) {
+//     echo "......................";
+//     echo "<br>";
+//     echo $item;
+//     echo "<br>";
+//     echo $menu_item_names[$i];
+//     echo "<br>";
+//     echo $menu_item_descriptions[$i];
+//     echo "<br>";
+//     echo $image_urls[$i];
+//     echo "<br>";
+//     echo rand(1 * 10, 70 * 10) / 10;
+//     echo "<br>";
+//     //$menu_items--;
+//     //if($menu_items == 0) break;
+// }
 
-//get and download images
-//  foreach ($filenames as $index=>$image){
-//      file_put_contents("../img/scraping_img/".$image.".jpg", file_get_contents($image_urls[$index]));
-//  }
+// //get and download images
+// //  foreach ($filenames as $index=>$image){
+// //      file_put_contents("../img/scraping_img/".$image.".jpg", file_get_contents($image_urls[$index]));
+// //  }
 
-// Reference: https://medoo.in/api/insert
+// // Reference: https://medoo.in/api/insert
 
-for ($i = 0; $i < 20; $i++) {
-    if ($i == 7 || $i == 8 || $i == 9 || $i == 10 || $i == 12 || $i == 13 || $i == 14 || $i == 15 || $i == 18 || $i == 19) {
-    } else {
-        $rand = rand(1 * 10, 70 * 10) / 10;
-        $database->insert("tb_dish", [
-            "id_category" => $id_category,
-            "name_l" => $menu_item_names[$i],
-            "description" => $menu_item_descriptions[$i],
-            "img_recorted" => $image_urls[$i],
-            "price" => $rand
-        ]);
-    }
-}
+// for ($i = 0; $i < 20; $i++) {
+//     if ($i == 7 || $i == 8 || $i == 9 || $i == 10 || $i == 12 || $i == 13 || $i == 14 || $i == 15 || $i == 18 || $i == 19) {
+//     } else {
+//         $rand = rand(1 * 10, 70 * 10) / 10;
+//         $database->insert("tb_dish", [
+//             "id_category" => $id_category,
+//             "namel" => $menu_item_names[$i],
+//             "description" => $menu_item_descriptions[$i],
+//             "img_recorted" => $image_urls[$i],
+//             "price" => $rand
+//         ]);
+//     }
+// }
 ?>
 <!--  DRINKS -->
 <!--  DRINKS -->
