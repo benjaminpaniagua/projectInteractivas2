54b1bc166a0825774103a66bea64926694786ac2
Dana Valverde <dana.cardenascr@gmail.com>
2023-11-28 13:49:15 -0600

    profile complete 50% only needs css

--- profile.php
+++ profile.php
@@ -21,14 +21,37 @@
         require_once './database.php';
 
         // Reference: https://medoo.in/api/select
-        $items = $database->select("tb_order_resgistered", 'tb_order_resgistered.id_order_registered', [
+        $items = $database->select("tb_order_resgistered", '*', [
             "id_user" => 4,
         ]);
 
         ?>
     </header>
     <?php 
-        var_dump($items);
+        foreach($items as $item){
+            var_dump($item["id_order_registered"]);
+            $orders = $database->select("tb_order_dishes", [
+                "[>]tb_dish" => ["id_dish" => "id_dish"],
+            ], [
+                "tb_dish.id_dish",
+                "tb_dish.names",
+                "tb_dish.img_recorted",
+                "tb_dish.price",
+                "tb_order_dishes.amoun_dish",
+            ], [
+                "id_order_registered" => $item["id_order_registered"]
+            ]);
+
+            foreach($orders as $index => $order){
+                var_dump($order);
+            }
+            echo "<br>";
+            echo "<br>";
+            echo "<br>";
+
+
+
+        }
     ?>
 
 
